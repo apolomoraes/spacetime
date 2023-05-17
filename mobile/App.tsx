66f0f9cba26e179ec3186a4b79e94e746deb6244
Apolo Moraes <vlogsapolo@gmail.com>
2023-05-16 23:53:22 -0300

    initial project structure

---
+++ mobile/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='bg-gray-950 flex-1 items-center justify-center'>
+      <Text className='text-gray-50 font-bold text-5xl'>Rocketseat</Text>
+      <StatusBar style="light" translucent />
+    </View>
+  );
+}
---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const app = fastify();
+const prisma = new PrismaClient();
+
+app.get("/users", async () => {
+  const users = await prisma.user.findMany();
+  return users;
+})
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log("HTTP server running on PORT 3333");
+})
